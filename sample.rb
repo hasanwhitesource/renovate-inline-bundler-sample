734688cc820948d0c6ca87bfb8515e4b89cc18c0
Matt Critchlow <matt@critchlow.xyz>
2022-02-07 08:51:36 -0800

    Setup sample repo for Renovate folks

---
+++ sample.rb
@@ -0,0 +1,15 @@
+require "bundler/inline"
+require "logger"
+
+logger = Logger.new($stdout)
+logger.level = ENV.fetch("LOG_LEVEL") { Logger::INFO }
+
+gemfile do
+  source "https://rubygems.org"
+
+  gem "bunny", "2.18.0"
+  gem "pg", "1.3.0"
+  gem "sequel", "5.51.0"
+end
+
+logger.info("Loaded all gems!")
